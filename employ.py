ee68faedf4b2fe406affd4c36513c4a08b437155
Rohit Saini <72138521+007rohitSaini@users.noreply.github.com>
2022-02-11 18:49:58 +0530

    Update employ.py

--- employ.py
+++ employ.py
@@ -6,8 +6,10 @@ def main():
     Enter showStock to display stock table.
     Enter sale to show sale table.
     Enter Sell to make transition.
-    Enter close to close the program
+    Enter help to see the menu.
+    Enter close to close the program.
     '''
+    print(menu)
     while True:
         init = input(':-> ')
         if init.lower() == 'sell':
@@ -16,9 +18,11 @@ def main():
             break
         elif init.lower() == 'showstock':
             connect.show_db()  
+        elif init.lower() == 'help':
+            print(menu) 
         elif init.lower() == 'showlogin':
             connect.show_login()
         elif init.lower() == 'sale':
             connect.show_sale()    
         else:
-            print('Input error, Try again')    
+            print('Input error, Try again')   
